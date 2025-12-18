57bc0bc41948e8e04ad5377499c07d1a0a1fad2c
Cristian8-8 <cristianemanuelsao85@gmail.com>
2025-12-18 11:51:50 -0300

    Add files via upload

---
+++ p/p1.py
@@ -0,0 +1,21 @@
+import pygame
+from pygame.locals import *
+from sys import exit
+
+pygame.init()
+
+tela = pygame.display.set_mode((640,480))
+
+while True:
+    
+    for event in pygame.event.get():
+         if event.type == QUIT:
+            pygame.quit()
+            exit()
+    pygame.draw(tela,40,30,100,50)
+    
+    
+    
+    
+    
+    pygame.display.update()
